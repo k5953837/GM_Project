76ea525c79e5682086fc44e81cba572d76d34188
kitb <k5953837@gmail.com>
2016-08-23 15:36:40 +0800

    Added function which user can remove one of shopping cart items.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,10 @@
 class Cart < ActiveRecord::Base
   # if Cart is destoryed, the associated objects(cart_item) are destroyed
   # alongside this object by calling their destroy method
-  has_many :cart_items, :dependent => :destroy
+  has_many :cart_items, dependent: :destroy
 
   # items (product here) is associated with cart_items
-  has_many :items, :through => :cart_items, :source => :product
+  has_many :items, through: :cart_items, source: :product
 
   def find_cart_item(product)
     cart_items.find_by(product_id: product)
