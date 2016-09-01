ea35f91d23161f43c2fd9f525f4597d68806246f
kitb <k5953837@gmail.com>
2016-09-01 17:13:12 +0800

    Generate mailer OrderMailer class

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,8 @@ class OrdersController < ApplicationController
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
       current_cart.cart_items.destroy_all
+      OrderMailer.notify_order_placed(@order).deliver!
+
       redirect_to order_path(@order.token)
     else
       render "carts/checkout"
